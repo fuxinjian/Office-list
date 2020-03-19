91b04f6feb3ae74b5d3b295a65acdd503d45dbcf
fuxinjian <54524872+fuxinjian@users.noreply.github.com>
2020-03-19 18:35:32 +0800

    Add files via upload

---
+++ js/todolist.js
@@ -0,0 +1,84 @@
+$(function () {
+  // 1 按下回车 把数据 存储到本地 里面
+  $("#title").on("keydown", function (e) {
+    if (e.keyCode === 13) {
+      if ($(this).val() === "") {
+        alert("请输入内容");
+      } else {
+        //读取本地 存储的数据
+        var local = getDate();
+        // 把local 数组进行更新 把最新的数据 追加到 local 后面
+        local.push({ title: $(this).val(), done: false });
+        // 把这个数组local 存到本地储存
+        savaDate(local);
+        load();
+        $(this).val("");
+      }
+    }
+  });
+  //删除模块
+  $("ol,ul").on("click", "a", function () {
+    //获取本地数据
+    var data = getDate();
+    //修改数据
+    var index = $(this).attr("id");
+    // 保存到本地
+    // 从第几个开始删 删多少个
+    data.splice(index, 1);
+    savaDate(data);
+    // 重新渲染
+    load();
+  });
+  //正在进行 和 已经完成
+  $("ol,ul").on("click", "input", function () {
+    //获取本地数据
+    var data = getDate();
+    //修改数据
+    var index = $(this).siblings("a").attr("id");
+    data[index].done = $(this).prop("checked");
+
+
+    // 保存到本地
+    savaDate(data);
+    // 重新渲染
+    load();
+  })
+  //读取本地存储的数据
+  function getDate() {
+    var data = localStorage.getItem("todolist");
+    if (data != null) {
+      //不为空 就返还
+      return JSON.parse(data);
+    } else {
+      return [];
+    }
+  }
+  //保存本地数据
+  function savaDate(data) {
+    localStorage.setItem("todolist", JSON.stringify(data));
+  }
+  //渲染加载数据
+  function load() {
+    //读取本地存储的数据
+    var data = getDate();
+    // 遍历清空 ol 里面元素的内容
+    $("ol,ul").empty();
+    var todoCount = 0;//正在完成的个数
+    var doneCount = 0;//已经完成的个数
+    // 遍历这个数据
+    $.each(data, function (i, n) {
+      if (n.done) {
+        // i为索引号，n为数据
+        $("ul").prepend("<li ><input type='checkbox' checked='checked'><p>" + n.title + "</p><a id=" + i + " href='javascript:''></a></li>");
+        doneCount++;
+      } else {
+        // i为索引号，n为数据
+        $("ol").prepend("<li ><input type='checkbox'><p>" + n.title + "</p><a id=" + i + " href='javascript:''></a></li>");
+
+        todoCount++;
+      }
+    });
+    $("#todocount").text(todoCount);
+    $("#donecount").text(doneCount);
+  }
+});
